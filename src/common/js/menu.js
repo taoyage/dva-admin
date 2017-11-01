a168c3569f06c069387e0effa70032fd2e2f5c7c
taoyage <tougakaku@gmail.com>
2017-11-01 17:04:39 +0800

    new branch

--- src/common/js/menu.js
+++ src/common/js/menu.js
@@ -1,21 +1,5 @@
 export default {
   menu: [{
-    id: '1',
-    icon: 'laptop',
-    name: '发行管理'
-  }, {
-    id: '10',
-    mpid: '1',
-    icon: 'laptop',
-    name: '产品列表',
-    route: '/product-list'
-  }, {
-    id: '12',
-    mpid: '1',
-    icon: 'laptop',
-    name: '产品列表2',
-    route: '/product'
-  }, {
     id: '2',
     icon: 'file',
     name: '文档管理'
